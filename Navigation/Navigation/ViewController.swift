3be3eebc054aeb47d4766173c7b09d0996df0398
ZhMks <142295043+ZhMks@users.noreply.github.com>
2023-08-26 17:32:30 +0400

    16
    Initial commit. Added tabbarView + UINavigationView + NavigationViewControllers

---
+++ Navigation/Navigation/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Максим Жуин on 26.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+        title = "TEST"
+        // Do any additional setup after loading the view.
+    }
+   
+}
+
+
